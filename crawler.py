dbb1f8297cbc6df71a746cb21ac9bb49100f0be8
matthew <matthew@Xiaodans-iMac.local>
2018-05-01 19:06:07 -0700

    update

--- crawler.py
+++ crawler.py
@@ -9,7 +9,7 @@ import gc
 
 import os
 import psutil
-import threading
+from multiprocessing import Process, Manager
 
 
 
@@ -22,9 +22,10 @@ class WebPage(QtWebEngineWidgets.QWebEnginePage):
         
 
 
-    def setHelperPara(self, categoryName, outfile):
+    def setHelperPara(self, categoryName, outfile, target_url):
         self.categoryName = categoryName
         self.outfile = outfile
+        self.cur_product_array = target_url
 
     def setCategoryDic(self, search_dic, category_id):
         self.search_dic = search_dic
@@ -49,10 +50,7 @@ class WebPage(QtWebEngineWidgets.QWebEnginePage):
         url = self.url().toString()
         # do stuff with html...
         if self.flag_download:
-            module.downloadProduct(html, url, self.outfile, self.categoryName, self.search_dic, self.category_id) 
-            print "[ DM_info ] before collect " + str(process.memory_info()[0])
-            gc.collect()
-            print "[ DM_info ] after collect " + str(process.memory_info()[0])
+            module.downloadProduct(html, url, self.outfile, self.categoryName, self.search_dic, self.category_id)
         else:
             self.getProductUrlArray(html)
 
@@ -72,7 +70,6 @@ class WebPage(QtWebEngineWidgets.QWebEnginePage):
     def getProductUrlArray(self, html):
         soup = bs.BeautifulSoup(html, 'html.parser')
         self.cur_product_array += ["https://www.crateandbarrel.com"+e['href'] for e in soup.select("a.product-miniset-thumbnail")]
-        
     
 
 if __name__ == '__main__':
@@ -81,41 +78,48 @@ if __name__ == '__main__':
     search_dic, category_id = Util.category()
     outfile = open('result.json', 'a')
     outfile.write('[\n')
-    target_url = []
+    target_url = Manager().list([])
+    # target_url = []
 
-    def cate_worker(search_dic, category_id, cate, outfile, start_urls):
+    def cate_worker(search_dic, category_id, cate, outfile, start_urls, target_url):
+
         app = QtWidgets.QApplication(sys.argv)
         webpage = WebPage()
         webpage.setCategoryDic(search_dic, category_id)
-        process = psutil.Process(os.getpid())
-        webpage.setHelperPara(cate, outfile)
-
+        webpage.setHelperPara(cate, outfile, target_url)
         webpage.start(start_urls[cate], False)
-        global target_url
+
         target_url = list(set(webpage.cur_product_array))
+
         sys.exit(app.exec_())
 
     def download_worker(search_dic, category_id, cate, outfile, target_url):
+
         app = QtWidgets.QApplication(sys.argv)
         webpage = WebPage()
         webpage.setCategoryDic(search_dic, category_id)
-        process = psutil.Process(os.getpid())
-        webpage.setHelperPara(cate, outfile)
+        webpage.setHelperPara(cate, outfile, [])
         webpage.start(target_url, True)
-        sys.exit(app.exec_())
+        sapp.exec_()
 
-    for cate in start_urls:
+    # for cate in start_urls:
 
-        print "[ DM_info ] " + cate
-        worker1 = threading.Thread(target=cate_worker, args=(search_dic, category_id, cate, outfile, start_urls,))
-        worker1.start()
-        worker1.join()
+    #     print "[ DM_info ] " + cate
+    #     worker1 = Process(target=cate_worker, args=(search_dic, category_id, cate, outfile, start_urls, target_url,))
+    #     worker1.start()
+    #     worker1.join()
+
+    #     print "[ DM_info ] start downloading product for " + cate
+    #     worker2 = Process(target=download_worker, args=(search_dic, category_id, cate, outfile, target_url,))
+    #     worker2.start()
+    #     worker2.join()
 
 
-        print "[ DM_info ] start downloading product for " + cate
-        worker2 = threading.Thread(target=download_worker, args=(search_dic, category_id, cate, outfile, target_url,))
-        worker2.start()
-        worker2.join()
-
-
+    # worker1 = Process(target=cate_worker, args=(search_dic, category_id, 'living', outfile, start_urls, target_url,))
+    # worker1.start()
+    # worker1.join()
+    # target_url = Manager().list([u'https://www.crateandbarrel.com/copper-wall-clock/s687032', u'https://www.crateandbarrel.com/malvern-cheval-floor-mirror/s638707'])
+    # worker2 = Process(target=download_worker, args=(search_dic, category_id, 'living', outfile, target_url))
+    # worker2.start()
+    # worker2.join()
 
